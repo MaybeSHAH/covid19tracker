74c0936a716ce97dacf1d7eae8bcc16e985c28cc
Liyakat Shaikh <liyakatshaikh07@gmail.com>
2021-03-16 00:16:39 +0530

    fetchCountries under progress

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { Line , Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
 const Chart = () => {
-    const { dailyData, setDailyData} = useState({});
+    const [ dailyData, setDailyData ] = useState([]);
 
     useEffect(()=>{
         const fetchAPI = async() =>{
@@ -12,17 +12,34 @@ const Chart = () => {
         }
         fetchAPI();
     });
+
     const lineChart = (
-        dailyData[0] ? (<Line
+        dailyData.length ? (<Line
             data={{
-            labels: dailyData(({ date }) => date),
-            datasets: [{},{}],
+            labels: dailyData.map(({ date }) => date),
+            datasets: [
+                {
+                    data: dailyData.map(({ confirmed }) => confirmed),
+                    label: 'Infected',
+                    borderColor: '#3333ff',
+                    fill: true,
+                },
+                {
+                    data: dailyData.map(({ deaths }) => deaths),
+                    label: 'Deaths',
+                    borderColor: 'red',
+                    backgroundColor: 'rgba(255,0,0,0.5)',
+                    fill: true,
+                }
+            ],
             }}
         />) : null
-    )
+    );
 
     return (
-        <h1> Chart</h1>
+        <div className={styles.container}>
+            { lineChart }
+        </div>
     )
 }
 
